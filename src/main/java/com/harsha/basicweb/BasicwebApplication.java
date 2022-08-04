cf54a0f2516b277fc4e6d9baca48402dd02a675b
h.rao.alleni <h.rao.alleni@HDC3-L-207LSFN.dir.svc.accenture.com>
2022-08-04 13:52:41 +0530

    2nd commit

--- src/main/java/com/harsha/basicweb/BasicwebApplication.java
+++ src/main/java/com/harsha/basicweb/BasicwebApplication.java
@@ -3,11 +3,14 @@ package com.harsha.basicweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class BasicwebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicwebApplication.class, args);
+		System.out.println();
 	}
 
 }
